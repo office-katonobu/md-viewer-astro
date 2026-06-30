802f11a7fdc94c1760d9a2736b36a5e0c3871c3d
Nobuo Kato <katonobu@office-katonobu.com>
2026-06-30 10:50:58 +0000

    step1-2動作確認済

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,8 @@
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite'; // インポートを追加
 
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()], // Viteのプラグインとして登録
+  },
+});
