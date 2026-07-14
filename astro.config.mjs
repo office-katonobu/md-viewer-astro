5f1ac053eff55de417bb9d6e166febe9e3665af1
Nobuo Kato <katonobu@office-katonobu.com>
2026-07-14 13:49:21 +0000

    DevToolber表示抑制

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,10 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   // 環境変数 BASE_PATH があればそれを、なければデフォルトで '/' を使用
   base: process.env.BASE_PATH || '/',  
+  // 開発ツールバーを無効化する設定
+  devToolbar: {
+    enabled: false,
+  },  
   vite: {
     plugins: [tailwindcss()],
 		server: {
